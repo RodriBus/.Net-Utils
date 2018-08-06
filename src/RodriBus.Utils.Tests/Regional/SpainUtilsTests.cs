0fb771c1e5de660ba03f631da82a43f1095a8c81
Diego Rodríguez Suárez-Bustillo <diego@rodribus.com>
2018-08-06 21:16:32 +0200

    Add spain region utils

---
+++ src/RodriBus.Utils.Tests/Regional/SpainUtilsTests.cs
@@ -0,0 +1,50 @@
+﻿using RodriBus.Utils.Regional;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace RodriBus.Utils.Tests.Regional
+{
+    public class SpainUtilsTests
+    {
+        [Theory]
+        [InlineData("34801337Z")]
+        [InlineData("45517999X")]
+        [InlineData("04909603T")]
+        [InlineData("14323708K")]
+        public static void ShouldValidateNif(string validId)
+        {
+            SpainUtils.IsValidIdNumber(SpainIdNumber.NIF, validId).Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("14801337Z")]
+        [InlineData("25517999X")]
+        [InlineData("304909603T")]
+        [InlineData("4423708K")]
+        public static void ShouldNotValidateInvalidNif(string invalidId)
+        {
+            SpainUtils.IsValidIdNumber(SpainIdNumber.NIF, invalidId).Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("Y6950597B")]
+        [InlineData("X4676338R")]
+        [InlineData("Z1555507E")]
+        public static void ShouldValidateNie(string validId)
+        {
+            SpainUtils.IsValidIdNumber(SpainIdNumber.NIE, validId).Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("Z6950597B")]
+        [InlineData("X467638R")]
+        [InlineData("Z15559507E")]
+        public static void ShouldNotValidateInvalidNie(string invalidId)
+        {
+            SpainUtils.IsValidIdNumber(SpainIdNumber.NIE, invalidId).Should().BeFalse();
+        }
+    }
+}
---
+++ src/RodriBus.Utils/Regional/SpainUtils.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RodriBus.Utils.Regional
+{
+    //http://www.interior.gob.es/web/servicios-al-ciudadano/dni/calculo-del-digito-de-control-del-nif-nie
+
+    /// <summary>
+    /// Reflecs the different types of identification numbers in Spain. 
+    /// </summary>
+    public enum SpainIdNumber
+    {
+        /// <summary>
+        /// This is the generic term for the tax ID number for all individuals.
+        /// For Spaniards, it's the DNI plus one letter; for foreigners the NIE must be used.
+        /// </summary>
+        NIF,
+
+        /// <summary>
+        /// This is the identification number for foreigners in Spain gave by the immigration service.
+        /// </summary>
+        NIE,
+    }
+
+    /// <summary>
+    /// Contains utils related to especific topics in Spain such as Id number and phone validations
+    /// </summary>
+    public static class SpainUtils
+    {
+        /// <summary>
+        /// Validates if a document number is valid according to spanish rules.
+        /// </summary>
+        /// <param name="type">The type of the document</param>
+        /// <param name="documentId">The Id number</param>
+        /// <returns></returns>
+        public static bool IsValidIdNumber(SpainIdNumber type, string documentId)
+        {
+            switch (type)
+            {
+                case SpainIdNumber.NIF:
+                    return _validateNif(documentId);
+                case SpainIdNumber.NIE:
+                    return _validateNie(documentId);
+            }
+            return false;
+        }
+
+        private static bool _validateNif(string doc)
+        {
+            var chars = "trwagmyfpdxbnjzsqvhlcke";
+
+            doc = doc.ToLower();
+
+            var groups = Regex.Match(doc, @"^(\d+)([a-zA-Z])$").Groups;
+            var numStr = groups[1].Value;
+            var controlChar = groups[2].Value;
+            if (int.TryParse(numStr, out int num))
+            {
+                var i = num % 23;
+                return controlChar.Equals(chars[i].ToString());
+            }
+            return false;
+        }
+
+        private static bool _validateNie(string doc)
+        {
+            var groups = Regex.Match(doc, @"^([a-zA-Z])(\d+)([a-zA-Z])$").Groups;
+            var controlDigitStr = groups[1].Value.ToLower().Replace("x", "0").Replace("y", "1").Replace("z", "2");
+            return groups.Count == 4 && _validateNif(controlDigitStr + groups[2] + groups[3]);
+        }
+    }
+}
