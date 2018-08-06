93b80e9b33efa0f4d1e91cce74692125f4c5ca1a
Diego Rodríguez Suárez-Bustillo <diego@rodribus.com>
2018-08-06 19:50:17 +0200

    Add TimeUtils

---
+++ src/RodriBus.Utils.Tests/TimeUtilsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Rodribus.Utils;
+using Xunit;
+
+namespace RodriBus.Utils.Tests
+{
+    public static class TimeUtilsTests
+    {
+        [Fact]
+        public static void ShouldTransformDateTimeToUnixTimestamp()
+        {
+            var date = new DateTime(2018, 08, 06, 0, 0, 0, DateTimeKind.Utc);
+            var expected = 1533513600;
+            var actual = TimeUtils.DateTimeToUnixTimestamp(date);
+
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public static void ShouldTransformUnixTimeStampToDateTime()
+        {
+            var timestamp = 1533513600;
+            var expected = new DateTime(2018, 08, 06, 0, 0, 0, DateTimeKind.Utc);
+            var actual = TimeUtils.UnixTimeStampToDateTime(timestamp);
+
+            actual.Should().Be(expected);
+        }
+    }
+}
---
+++ src/RodriBus.Utils/TimeUtils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RodriBus.Utils
+{
+    public static class TimeUtils
+    {
+        /// <summary>
+        /// Converts <see cref="DateTime"/> instance into UNIX timestamp (total seconds elapsed since 1970-01-01 at 00:00:00:000).
+        /// </summary>
+        /// <remarks>It uses UTC for time calculations.</remarks>
+        /// <param name="dateTime">The <see cref="DateTime"/> instance</param>
+        /// <returns>Time in seconds</returns>
+        public static int DateTimeToUnixTimestamp(DateTime dateTime)
+        {
+            return (int)(TimeZoneInfo.ConvertTime(dateTime, TimeZoneInfo.Utc) -
+                   new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds;
+        }
+
+        /// <summary>
+        /// Converts a UNIX timestamp into a <see cref="DateTime"/> instance.
+        /// </summary>
+        /// <remarks>It uses UTC for time calculations.</remarks>
+        /// <param name="unixTimeStamp">Time in seconds</param>
+        /// <returns>The <see cref="DateTime"/> instance</returns>
+        public static DateTime UnixTimeStampToDateTime(int unixTimeStamp)
+        {
+            var dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp);
+            return dtDateTime;
+        }
+    }
+}
